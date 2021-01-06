1969808fa2d0cfee90a30359fb08dc186f89e884
Luka-Emra <lukaemrashvili93@gmail.com>
2021-01-06 17:35:27 +0400

    feat: add final version

---
+++ Generic_Set.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+// Luka Emrashvili
+// Red ID: 823 355 800
+
+namespace Set
+{
+    public delegate bool F<T>(T elt);       // declare delegate
+
+    public class Generic_Set<T> : IEnumerable<T>
+    {
+        protected List<T> Collection;
+
+        public bool any
+        {
+            get
+            {
+                return !Collection.Any();
+            }
+        }
+
+        public int x
+        {
+            get
+            {
+                return Collection.Count();
+            }
+        }
+
+        public Generic_Set()
+        {
+            Collection = new List<T>();
+        }
+
+        public Generic_Set(IEnumerable<T> elements)
+        {
+            Collection = new List<T>();
+
+            foreach (var el in elements)
+            {
+                if (!Collection.Contains(el))  // check if the element is in the set
+                {
+                    Collection.Add(el);
+                }
+            }
+        }
+
+        public static Generic_Set<T> operator +(Generic_Set<T> left_side, Generic_Set<T> right_side)    // creating operator
+        {
+            Generic_Set<T> U = new Generic_Set<T>(left_side);
+
+            foreach (var x in right_side)
+            {
+                if (!U.Containing(x))
+                {
+                    U.Collection.Add(x);
+                }
+            }
+
+            return U;
+        }
+
+        public virtual bool Addition(T i)    // Addition method
+        {
+            if (!Collection.Contains(i))
+            {
+                Collection.Add(i);
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual bool Removal(T i)            // Removal method
+        {
+            if (Collection.Contains(i))
+            {
+                Collection.Remove(i);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Containing(T i) => Collection.Contains(i);      // checking method
+
+        public Generic_Set<T> Filtering(F<T> fil_func)             // Filtering Method
+        {
+            Generic_Set<T> fil_set = new Generic_Set<T>();
+
+            foreach (var x in Collection)
+            {
+                if (fil_func(x))
+                {
+                    fil_set.Collection.Add(x);
+                }
+            }
+
+            return fil_set;
+        }
+
+        public IEnumerator<T> GetEnumerator()      // implement IEnumerable
+        {
+            foreach (var x in Collection)
+            {
+                yield return x;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    public class Sorted<T> : Generic_Set<T>        // sorted class, subclass of Generic_Set()
+           where T : IComparable<T>
+    {
+        public Sorted(IEnumerable<T> element)
+                : base(element)
+        {
+            Collection.Sort();
+        }
+
+        public override bool Addition(T i)
+        {
+            if (!Collection.Contains(i))
+            {
+                foreach (var x in Collection)
+                {
+                    if (i.CompareTo(x) < 0)
+                    {
+                        Collection.Insert(Collection.IndexOf(x), i);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        public override bool Removal(T i)     // overriden method
+        {
+            return base.Removal(i);
+        }
+
+        public static Sorted<T> operator +(Sorted<T> left_hand, Sorted<T> right_hand)
+        {
+            Sorted<T> U = new Sorted<T>(left_hand);
+            foreach (var x in right_hand)
+            {
+                if (!left_hand.Containing(x))
+                {
+                    U.Addition(x);
+                }
+            }
+            return U;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using Set;
+
+// Luka Emrashvili
+// Red ID: 823 355 800
+
+namespace Assignment4
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t\t-----Testing Default Constructor-----\n");
+            Generic_Set<int> First_Set = new Generic_Set<int>();
+            Console.WriteLine("\t\tIs it True or False that the First Set is Empty? It's : " + First_Set.any);
+            First_Set.Addition(7);
+            First_Set.Addition(2);
+            First_Set.Addition(16);
+            First_Set.Addition(12);
+
+            Console.WriteLine("\t\tWhat about now? it's: " + First_Set.any);
+            Console.Write("\t\tThese elements have been added to the First set : ");
+
+            Console.Write("{ ");
+            foreach (var x in First_Set)
+            {
+                Console.Write(x + ",  ");
+            }
+            Console.Write("}");
+
+            List<int> set = new List<int>();
+            set.Add(20);
+            set.Add(44);
+            set.Add(12);
+            set.Add(67); 
+     
+            Console.WriteLine("\n\n\t\t-----Testing Parameterized constructor-----\n");
+            Generic_Set<int> mySet2 = new Generic_Set<int>(set);
+            Console.WriteLine("\t\tIs it True or False that the Second Set is Empty ? It's : " + mySet2.any);
+            Console.Write("\t\tThese elements are included in the Second set: ");
+
+            Console.Write("{ ");
+            foreach (var x in mySet2)
+            {
+                Console.Write(x + ",  ");
+            }
+            Console.Write("}");
+
+
+            Console.Write("\n\n\t\t-----Union: ");
+            Generic_Set<int> U = First_Set + mySet2;
+
+            Console.Write("{ ");
+            foreach (var x in U)
+            {
+                Console.Write(x + ",  ");
+            }
+            Console.Write("}");
+
+            Console.WriteLine("\n\t\tCan we remove 44? : " + U.Removal(44));
+            Console.WriteLine("\t\tCan we remove 88? : " + U.Removal(88));
+            Console.Write("\n\t\tNew Set : ");
+
+
+            Console.Write("{ ");
+            foreach (var x in U)
+            {
+                Console.Write(x + ",  ");
+            }
+            Console.Write("}");
+
+            static bool Greater_Than_Fifteen(int x)
+            {
+                return (x > 15) ? true : false;
+            }
+
+            Console.Write("\n\n\t\tFiltered Set with numbers > 15 from the Union Set : ");
+            Generic_Set<int> Fil1 = U.Filtering(Greater_Than_Fifteen);
+
+            Console.Write("{ ");
+            foreach (var x in Fil1)
+            {
+                Console.Write(x + ",  ");
+            }
+            Console.Write("}");
+
+            Console.Write("\n\n\t\tSorted Set : ");
+            var sort = new Set.Sorted<int>(Fil1);
+
+            Console.Write("{ ");
+            foreach (var x in Fil1)
+            {
+                Console.Write(x + ",  ");
+            }
+            Console.Write("}");
+
+            Console.Write("\n\t\tAfter adding 24 : ");
+            sort.Addition(24);
+
+            Console.Write("{ ");
+            foreach (var x in sort)
+            {
+                Console.Write(x + ",  ");
+            }
+            Console.Write("}");
+
+            static bool Fil2(int x)
+            {
+                return (x <= 15) ? true : false;
+            }
+
+            Console.Write("\n\n\t\tSecond Set with numbers less than or equal to 15 : ");
+            Generic_Set<int> fil2 = U.Filtering(Fil2);
+
+            Console.Write("{ ");
+            foreach (var x in fil2)
+            {
+                Console.Write(x + ",  ");
+            }
+            Console.Write("}");
+
+            Console.Write("\n\n\t\tSort the second set : ");
+            var sort2 = new Set.Sorted<int>(fil2);
+
+            Console.Write("{ ");
+            foreach (var x in sort2)
+            {
+                Console.Write(x + ",  ");
+            }
+            Console.Write("}\n\n\n");
+        }
+    }
+}
